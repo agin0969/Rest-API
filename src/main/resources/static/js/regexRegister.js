932af698c0b2e9205f8a28d3244f9fb31c75ad68
le trong thien <letrogthien@gmail.com>
2024-01-30 19:52:51 +0700

    gin

--- src/main/resources/static/js/regexRegister.js
+++ src/main/resources/static/js/regexRegister.js
@@ -1,4 +1,8 @@
-function showAlert() {
-	var passwordPatternDiv = document.getElementById("passwordPattern");
+function showAlert(name) {
+	var passwordPatternDiv = document.getElementById(name);
 	passwordPatternDiv.style.display = "block";
+}
+function hideAlert(){
+	var a = document.getElementById("btn-alert")
+	a.style.display="none";
 }
